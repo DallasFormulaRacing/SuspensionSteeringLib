84474ba30ba2ec9021c27023256add5f8a0e2d66
GHAFHA <45609663+GHAFHA@users.noreply.github.com>
2023-12-18 14:07:44 -0600

    initial commit

--- conversion_equations/conversions.py
+++ conversion_equations/conversions.py
@@ -1,2 +1,37 @@
+import pandas
+from pandas import DataFrame
+
+
 class conversions:
-    pass
+
+    LINPOT_CONVERSION_CONSTANT = 15.0
+    LINPOT_CONVERSION_OFFSET = 75.0
+    ACCEL_G_CONSTANT = 1.0
+
+    def __init__(self):
+        df_accel = pandas.read_csv("")  # base volatage for accelerometer
+        # base volatage for linear potentiometers
+        def_linpots = pandas.read_csv("")
+        # initlaize with base line voltages for x, y, and z
+
+    def calculate_acceleration_conversion_factors(self):
+        # calculate the variables for each axis
+        pass
+
+    def calculate_baseline_accel(self, x, y, z):
+        # average the first 20 values from each axis
+        # calculate the variable for each axis
+        pass
+
+    def convert_to_gs(self):
+        # convert the voltage to gs
+        pass
+
+    def convert_voltage_to_mm(self) -> DataFrame:
+        pass
+
+    def calculate_wheel_load(self):
+        pass
+
+    # def apply_low_pass_filter(self):
+    #     pass
---
+++ make_plots/plots.py
@@ -0,0 +1,20 @@
+import pandas
+import conversion_equations.conversions as conversions
+
+
+class plots:
+
+    def __init__(self):
+        pass
+
+    def plot_acceleration_vs_time(self):
+        pass
+
+    def def_plot_linpot_vs_time(self):
+        pass
+
+    def plot_wheel_load_vs_time(self):
+        pass
+
+    def plot_acceleration_vs_wheel_load(self):
+        pass

d38a7524ffb4c302fdc254ebdb6c0f21b21dd060
unknown <oxs200002@utdallas.edu>
2023-12-21 15:49:06 -0600

    changed usages of x to displacement

--- test/conftest.py
+++ test/conftest.py
@@ -5,9 +5,9 @@ import pytest
 def test_wheel_load_calculations():
     
     return WheelLoad(
-        x_LF = 0.0, 
-        x_RF = 0.0, 
-        x_RL = 0.0, 
-        x_RR = 0.0, 
+        displacement_LF = 0.0, 
+        displacement_RF = 0.0, 
+        displacement_RL = 0.0, 
+        displacement_RR = 0.0, 
         filename="data\output2_linpot_2023-10-14_13-23-28.csv"
         )
--- test/test_handler.py
+++ test/test_handler.py
@@ -1,6 +1,5 @@
 import pytest
 
-@pytest.fixture
 def test_constants(test_wheel_load_calculations):
     result = test_wheel_load_calculations.calculate_consts()
     assert(result[0] >= 243.10 and result[0] <= 243.5)

0269d896dd90237b8235d2d487ebd791cfa938f6
Ani2nem <ani2nem@gmail.com>
2024-01-27 15:31:18 -0600

    worked on functions in conversions.py and tried to use them in handler.py

--- conversion_equations/conversions.py
+++ conversion_equations/conversions.py
@@ -1,94 +1,146 @@
 import pandas as pd
-import numpy as np
-import mpmath
-from sympy import symbols, diff
 
-class conversions:
-    linpot_fr_conversion_factor = 15.0
-    linpot_fl_conversion_factor = 15.0
-    linpot_rr_conversion_factor = 15.0
-    linpot_rl_conversion_factor = 15.0
+class Conversions:
+    LINPOT_CONVERSION_FACTOR = 15.0
+    # dunno the damping coefficient yet, we need it
+    DAMIPING_COEFFICIENT = 0
 
-    def __init__(self) -> None:
-        # read in the data files
-        self.linpot_file_name = "output2_linpot_2023-10-14_13-23-28.csv"
+    def __init__(self, file_name : str):
+        # create linpot_data datafram for linpot file
+        self.file_name = file_name
         self.linpot_data = pd.DataFrame(pd.read_csv(self.linpot_file_name))
 
-        self.acc_file_name = "output2_analog_2023-10-14_13-18-15.csv"
-        self.acc_data = pd.DataFrame(pd.read_csv(self.acc_file_name))
+        # new data frame named displacements whhere the time column is removed, just displacements
+        self.displacements = pd.DataFrame(pd.read_csv(self.linpot_file_name).drop("Time"), unit="s")
+
 
         self.fr_basis = self.linpot_data.loc[0,"Front Right"]
         self.fl_basis = self.linpot_data.loc[0,"Front Left"]
         self.rr_basis = self.linpot_data.loc[0,"Rear Right"]
         self.rl_basis = self.linpot_data.loc[0,"Rear Left"]
 
-        self.x_basis = self.acc_data.loc[0,"X"]
-        self.y_basis = self.acc_data.loc[0,"Y"]
-        self.z_basis = self.acc_data.loc[0,"Z"]
+
+   # calculates voltage to mm using conversion factor.
+        # reads row-wise first, then moves to the next row.
+    def voltage_to_mm(self):
+        for i, row in self.linpot_data.iterrows():
+            self.linpot_data.loc[i, "Front Right"] = -(row["Front Right"] * self.LINPOT_CONVERSION_FACTOR) + 75.0
+            self.linpot_data.loc[i, "Front Left"] = -(row["Front Left"] * self.LINPOT_CONVERSION_FACTOR) + 75.0
+            self.linpot_data.loc[i, "Rear Right"] = -(row["Rear Right"] * self.LINPOT_CONVERSION_FACTOR) + 75.0
+            self.linpot_data.loc[i, "Rear Left"] = -(row["Rear Left"] * self.LINPOT_CONVERSION_FACTOR) + 75.0
 
 
+    # metod to calc. time constant, return time
+    def cal_time_const(self):
+        time_const = self.linpot_data.loc[0, "Time"] - self.linpot_data.loc[1, "Time"]
+        return time_const
+
+
+    # method to calculate displacement, returns a dataframe
+        # reads column-wise first, then moves to the next column.
+    def calc_displacements(self):
+         # calculated displacemnt from resting displacement of linpot
+        resting_displacement = self.displacements.head(4).mean()
+        for i in range(0, len(self.displacements)):
+            self.displacements.iloc[:, i] = self.displacements.iloc[:, i] - resting_displacement
+        return self.displacements
     
-    def voltage_to_mm(self, column_name: str) -> None:
-        # Check if the column exists in the DataFrame
-        if column_name in self.data.columns:
-            self.data[column_name] = self.data[column_name] * self.linpot_fr_conversion_factor
-        else:
-            print(f"Column '{column_name}' not found in the DataFrame.")
-    
 
 
-    def calculate_velocity(accelerations, time_intervals):
-        """
-        Calculate velocity from acceleration data.
-        :param accelerations: DataFrame of acceleration values.
-        :param time_intervals: DataFrame column for time values.
-        :return: List of velocity values. """
-
-        velocities = [0]  # starting with an initial velocity of 0
-        for i in range(1, len(accelerations)):
-            # Velocity = Previous Velocity + (Acceleration * Time Interval)
-            velocity = velocities[i-1] + accelerations[i] * time_intervals[i]
-            velocities.append(velocity)
-        return velocities
-
-    accelerations = [0.5, 0.6, 0.55, 0.4, 0.3]  # Accelerations in m/s^2
-    time_stamps = [0, 1, 2, 3, 4]  # Time stamps in seconds
-
-    time_intervals = np.diff(time_stamps)
-
-    velocities = calculate_velocity(accelerations, time_intervals)
-
-    print("Velocities:", velocities)
-
-
-
-    def calculate_velocity(displacements, time_stamps):
+    # Displacemnts is a dataFrame
+    def calculate_velocity(self, displacements, time_const):
         """
         Another method of calculating velocity from displacement data.
         :param displacements: List or array of displacement values. Displacement values will need to be calculated for this to work, this will be the difference in position between the starting point and its positions at each interval, this will also need to be a new DF
         :param time_stamps: List or array of time stamps.
         :return: List of velocity values.
         """
+
+        # FORMULA: Velociy = displacement / time_const
+
+        # create a velocities array
         velocities = []
+
+        # for the first row, displacement at the 1st calc. displacement is the delta between resing pos. and 1st reading. 
+        velocity = self.displacements.iloc[0] / time_const
+        velocities.append(velocity)
+
+        # do this for all rows in the main dataframe
         for i in range(1, len(displacements)):
-            delta_x = displacements[i] - displacements[i - 1]
-            delta_t = time_stamps[i] - time_stamps[i - 1]
+            delta_x = self.displacements.iloc[i] - self.displacements.iloc[i - 1]
+            delta_t = time_const
             velocity = delta_x / delta_t
             velocities.append(velocity)
         return velocities
+    
 
-    def estimate_damping_force(velocities, damping_coefficient):
+
+    # this is the force that will be on the damper
+    # WHat is the DAMPING_COEFFICIENT? 
+    def estimate_damping_force(velocities, DAMPING_COEFFICIENT):
         """
         Estimate the damping force using the damping coefficient and velocity.
 
         :param velocities: List or array of velocity values.
-        :param damping_coefficient: Damping coefficient (constant).
+        :param DAMPING_COEFFICIENT: Damping coefficient (constant).
         :return: List of estimated damping forces.
         """
-        damping_forces = [damping_coefficient * v for v in velocities]
+        damping_forces = [DAMPING_COEFFICIENT * v for v in velocities]
         return damping_forces
 
             
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+
+ # we need to find the force on the damper knowing the displacement
+    def displacement_to_force(self):
+         for i, row in self.data.iterrows():
+            # Assuming you have a function or formula to calculate force based on displacement
+            front_right_force = self.calculate_force(row["Front Right"])
+            front_left_force = self.calculate_force(row["Front Left"])
+            rear_right_force = self.calculate_force(row["Rear Right"])
+            rear_left_force = self.calculate_force(row["Rear Left"])
+
+            # Add the calculated forces to the DataFrame
+            self.data.loc[i, "Damper Force"] = {
+                "Front Right": front_right_force,
+                "Front Left": front_left_force,
+                "Rear Right": rear_right_force,
+                "Rear Left": rear_left_force
+            }
+        # just add ot the data frame
+         self.data.to_csv("./data/data/output2_linpot_2023-10-14_13-23-28.csv", index=False)
+
+
+    def calculate_force(self, displacement):
+        return displacement * 15
+
+
+
+
+
+
+
 
 
 
@@ -116,3 +168,4 @@ class conversions:
         return acceleration
 
     
+"""
--- handler.py
+++ handler.py
@@ -1,2 +1,18 @@
+import pandas as pd
+#imoport conversions class
+from conversion_equations.conversions import Conversions
+
+# create a conversions object from conversions class.
+
 class handler:
-    pass
+    # import init method
+    def __init__(self, file_name):
+        self.conversions = Conversions(file_name)
+
+    # method damper, utilize other methods and it should give velocities. 
+    def process_damper_data(self):
+        self.conversions.voltage_to_mm()
+        time_const = self.conversions.cal_time_const()
+        displacements = self.conversions.calc_displacements()
+        velocities = self.conversions.calculate_velocity(displacements, time_const)
+        damping_forces = self.conversions.estimate_damping_force(velocities, self.DAMPING_COEFFICIENT)

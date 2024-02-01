f0f4ca1378b5105931f8db156bf457d9425e0140
GHAFHA <45609663+GHAFHA@users.noreply.github.com>
2024-02-01 10:41:38 -0600

    updated to add accel conversion constants

--- calculations/calculations.py
+++ calculations/calculations.py
@@ -1,77 +1,88 @@
-import pandas
-import time
 from calculations import constant_enum
 
 constants = constant_enum.Constants
 
+
 class Calculations:
-    
+
     def calculate_forces_part_1(self) -> float:
-       return self.K_H * ( self.data["Front Left_lowpass"] + self.data["Front Right_lowpass"] + self.data["Rear Left_lowpass"] + self.data["Rear Right_lowpass"])
-    
+        return self.K_H * (self.data["Front Left_lowpass"] + self.data["Front Right_lowpass"] + self.data["Rear Left_lowpass"] +
+                           self.data["Rear Right_lowpass"])
+
     def calculate_forces_part_2(self) -> float:
-       return self.K_P * ( self.data["Front Left_lowpass"] + self.data["Front Right_lowpass"] - self.data["Rear Left_lowpass"] - self.data["Rear Right_lowpass"])
-      
+        return self.K_P * (self.data["Front Left_lowpass"] + self.data["Front Right_lowpass"] - self.data["Rear Left_lowpass"] -
+                           self.data["Rear Right_lowpass"])
+
     def calculate_forces_part_3_front(self) -> float:
-       return self.K_R * ( self.data["Front Left_lowpass"] - self.data["Front Right_lowpass"] + self.data["Rear Left_lowpass"] - self.data["Rear Right_lowpass"]) * self.q
+        return self.K_R * (self.data["Front Left_lowpass"] - self.data["Front Right_lowpass"] + self.data["Rear Left_lowpass"] -
+                           self.data["Rear Right_lowpass"]) * self.q
 
     def calculate_forces_part_3_rear(self) -> float:
-       return self.K_R * ( self.data["Front Left_lowpass"] - self.data["Front Right_lowpass"] + self.data["Rear Left_lowpass"] - self.data["Rear Right_lowpass"]) * (1 - self.q)
+        return self.K_R * (self.data["Front Left_lowpass"] - self.data["Front Right_lowpass"] + self.data["Rear Left_lowpass"] -
+                           self.data["Rear Right_lowpass"]) * (1 - self.q)
 
     def calculate_forces_part_4_front(self) -> float:
-       return self.K_W * ( self.data["Front Left_lowpass"] - self.data["Front Right_lowpass"] - self.data["Rear Left_lowpass"] + self.data["Rear Right_lowpass"]) * (1 - self.w)
-    
+        return self.K_W * (self.data["Front Left_lowpass"] - self.data["Front Right_lowpass"] - self.data["Rear Left_lowpass"] +
+                           self.data["Rear Right_lowpass"]) * (1 - self.w)
+
     def calculate_forces_part_4_rear(self) -> float:
-       return self.K_W * ( self.data["Front Left_lowpass"] - self.data["Front Right_lowpass"] - self.data["Rear Left_lowpass"] + self.data["Rear Right_lowpass"]) * self.w
+        return self.K_W * (self.data["Front Left_lowpass"] - self.data["Front Right_lowpass"] - self.data["Rear Left_lowpass"] +
+                           self.data["Rear Right_lowpass"]) * self.w
 
     def calculate_force_front_left(self) -> float:
-       self.data["Force Front Left"] = constants.CORNER_WEIGHT_LF + 0.25 * ( self.calculate_forces_part_1() + self.calculate_forces_part_2() + self.calculate_forces_part_3_front() + self.calculate_forces_part_4_front())
-       return self.data["Force Front Left"]
-    
+        self.data["Force Front Left"] = constants.CORNER_WEIGHT_LF + 0.25 * (self.calculate_forces_part_1(
+        ) + self.calculate_forces_part_2() + self.calculate_forces_part_3_front() + self.calculate_forces_part_4_front())
+        return self.data["Force Front Left"]
+
     def calculate_force_front_right(self) -> float:
-       self.data["Force Front Right"] = constants.CORNER_WEIGHT_RF + 0.25 * ( self.calculate_forces_part_1() + self.calculate_forces_part_2() - self.calculate_forces_part_3_front() - self.calculate_forces_part_4_front())
-       return self.data["Force Front Right"]
-    
+        self.data["Force Front Right"] = constants.CORNER_WEIGHT_RF + 0.25 * (self.calculate_forces_part_1(
+        ) + self.calculate_forces_part_2() - self.calculate_forces_part_3_front() - self.calculate_forces_part_4_front())
+        return self.data["Force Front Right"]
+
     def calculate_force_rear_right(self) -> float:
-       self.data["Force Rear Right"] = constants.CORNER_WEIGHT_RR + 0.25 * ( self.calculate_forces_part_1() - self.calculate_forces_part_2() + self.calculate_forces_part_3_rear() - self.calculate_forces_part_4_rear())
-       return self.data["Force Rear Right"]
-    
+        self.data["Force Rear Right"] = constants.CORNER_WEIGHT_RR + 0.25 * (self.calculate_forces_part_1(
+        ) - self.calculate_forces_part_2() + self.calculate_forces_part_3_rear() - self.calculate_forces_part_4_rear())
+        return self.data["Force Rear Right"]
+
     def calculate_force_rear_left(self) -> float:
-       self.data["Force Rear Left"] = constants.CORNER_WEIGHT_RL + 0.25 * ( self.calculate_forces_part_1() - self.calculate_forces_part_2() - self.calculate_forces_part_3_rear() - self.calculate_forces_part_4_rear())
-       return self.data["Force Rear Left"]
-    
+        self.data["Force Rear Left"] = constants.CORNER_WEIGHT_RL + 0.25 * (self.calculate_forces_part_1(
+        ) - self.calculate_forces_part_2() - self.calculate_forces_part_3_rear() - self.calculate_forces_part_4_rear())
+        return self.data["Force Rear Left"]
+
     def calculate_wheel_rate_front(self) -> float:
-     self.wheel_rate_front = (constants.SPRING_RATE_FRONT)/(constants.MOTION_RATIO_FRONT ** 2)
-     return self.wheel_rate_front
-   
+        self.wheel_rate_front = (constants.SPRING_RATE_FRONT)/(constants.MOTION_RATIO_FRONT ** 2)
+        return self.wheel_rate_front
+
     def calculate_wheel_rate_rear(self) -> float:
-     self.wheel_rate_rear = (constants.SPRING_RATE_REAR)/(constants.MOTION_RATIO_REAR ** 2)
-     return self.wheel_rate_rear
-   
+        self.wheel_rate_rear = (constants.SPRING_RATE_REAR)/(constants.MOTION_RATIO_REAR ** 2)
+        return self.wheel_rate_rear
+
     def calculate_wheel_rate_roll_front(self) -> float:
-     self.wheel_rate_roll_front = (constants.SPRING_RATE_ROLL_FRONT)/(constants.MOTION_RATIO_ROLL_FRONT ** 2)
-     return self.wheel_rate_roll_front
-   
+        self.wheel_rate_roll_front = (constants.SPRING_RATE_ROLL_FRONT)/(constants.MOTION_RATIO_ROLL_FRONT ** 2)
+        return self.wheel_rate_roll_front
+
     def calculate_wheel_rate_roll_rear(self) -> float:
-     self.wheel_rate_roll_rear = (constants.SPRING_RATE_ROLL_REAR)/(constants.MOTION_RATIO_ROLL_REAR ** 2)
-     return self.wheel_rate_roll_rear
-   
+        self.wheel_rate_roll_rear = (constants.SPRING_RATE_ROLL_REAR)/(constants.MOTION_RATIO_ROLL_REAR ** 2)
+        return self.wheel_rate_roll_rear
+
     def calculate_K_H_K_P(self) -> float:
-     self.K_H = 0.5 * ( self.wheel_rate_front + self.wheel_rate_rear )
-     self.K_P = self.K_H
-     return self.K_H
-   
+        self.K_H = 0.5 * (self.wheel_rate_front + self.wheel_rate_rear)
+        self.K_P = self.K_H
+        return self.K_H
+
     def calculate_K_R_K_W(self) -> float:
-     self.K_R = 0.5 * ( self.wheel_rate_front + 2 * self.wheel_rate_roll_front) + 0.5 * ( self.wheel_rate_rear + 2 * self.wheel_rate_roll_rear)
-     self.K_W = self.K_R
-     return self.K_R
-   
+        self.K_R = 0.5 * (self.wheel_rate_front + 2 * self.wheel_rate_roll_front) + \
+            0.5 * (self.wheel_rate_rear + 2 * self.wheel_rate_roll_rear)
+        self.K_W = self.K_R
+        return self.K_R
+
     def calculate_a_b(self) -> float:
-      self.wheel_load_const_a = 0.5 * ( self.wheel_rate_front + 2 * self.wheel_rate_roll_front) - 0.5 * ( self.wheel_rate_rear + 2 * self.wheel_rate_roll_rear)
-      self.wheel_load_const_b = self.wheel_load_const_a
-      return self.wheel_load_const_a
-   
+        self.wheel_load_const_a = 0.5 * (self.wheel_rate_front + 2 * self.wheel_rate_roll_front) - \
+            0.5 * (self.wheel_rate_rear + 2 * self.wheel_rate_roll_rear)
+        self.wheel_load_const_b = self.wheel_load_const_a
+        return self.wheel_load_const_a
+
     def calculate_q_w(self) -> float:
-     self.q = ((self.K_R + self.wheel_load_const_a) / (self.K_R - self.wheel_load_const_a)) - 1
-     self.w = self.q
-     return self.q
+        self.q = ((self.K_R + self.wheel_load_const_a) / (self.K_R - self.wheel_load_const_a)) - 1
+        self.w = self.q
+        return self.q
--- conversions/conversions.py
+++ conversions/conversions.py
@@ -1,16 +1,14 @@
 import pandas as pd
-from pandas import DataFrame
-import plotly.express as px
 import time
-from sklearn.cluster import KMeans
 from Filter import Filter
 from conversions import conversion_factor_enum
 
-# Add dampening to factor into the wheel loads. Depends on the velocity 
+# Add dampening to factor into the wheel loads. Depends on the velocity
 # Dampening is proportional to velocity and it's a linear relationship
 
+
 class Conversions:
-    
+
     LINPOT_CONVERSION_CONSTANT = 15.0
     LINPOT_CONVERSION_OFFSET = 75.0
     MM_TO_IN_CONVERSION_FACTOR = 0.0393701
@@ -20,12 +18,12 @@ class Conversions:
         self.filename = filename
         self.data = pd.DataFrame(pd.read_csv(filename))
         self.switch_columns()
-        
+
         print(self.data)
 
     def switch_columns(self):
         self.data = self.data.rename(
-            columns = {
+            columns={
                 "Front Right": "Front Left",
                 "Front Left": "Rear Left",
                 "Rear Left": "Front Right",
@@ -36,22 +34,32 @@ class Conversions:
     def convert_voltage_to_in(self):
         constants = conversion_factor_enum.Constants
         for i, row in self.data.iterrows():
-            self.data.loc[i, "Front Right"] = (-(row["Front Right"] * constants.LINPOT_CONVERSION_CONSTANT) + constants.LINPOT_CONVERSION_OFFSET) * constants.MM_TO_IN_CONVERSION_FACTOR
-            self.data.loc[i, "Front Left"] = (-(row["Front Left"] * constants.LINPOT_CONVERSION_CONSTANT) + constants.LINPOT_CONVERSION_OFFSET) * constants.MM_TO_IN_CONVERSION_FACTOR
-            self.data.loc[i, "Rear Right"] = (-(row["Rear Right"] * constants.LINPOT_CONVERSION_CONSTANT) + constants.LINPOT_CONVERSION_OFFSET) * constants.MM_TO_IN_CONVERSION_FACTOR
-            self.data.loc[i, "Rear Left"] = (-(row["Rear Left"] * constants.LINPOT_CONVERSION_CONSTANT) + constants.LINPOT_CONVERSION_OFFSET) * constants.MM_TO_IN_CONVERSION_FACTOR
+            self.data.loc[i, "Front Right"] = (-(row["Front Right"] * constants.LINPOT_CONVERSION_CONSTANT) +
+                                               constants.LINPOT_CONVERSION_OFFSET) * constants.MM_TO_IN_CONVERSION_FACTOR
+            self.data.loc[i, "Front Left"] = (-(row["Front Left"] * constants.LINPOT_CONVERSION_CONSTANT) +
+                                              constants.LINPOT_CONVERSION_OFFSET) * constants.MM_TO_IN_CONVERSION_FACTOR
+            self.data.loc[i, "Rear Right"] = (-(row["Rear Right"] * constants.LINPOT_CONVERSION_CONSTANT) +
+                                              constants.LINPOT_CONVERSION_OFFSET) * constants.MM_TO_IN_CONVERSION_FACTOR
+            self.data.loc[i, "Rear Left"] = (-(row["Rear Left"] * constants.LINPOT_CONVERSION_CONSTANT) +
+                                             constants.LINPOT_CONVERSION_OFFSET) * constants.MM_TO_IN_CONVERSION_FACTOR
 
     def clean_data(self):
         filter = Filter.Filter()
-        self.data = filter.butter_lowpass_filter(self.data, "Front Right", 4, 30, 2)
-        self.data = filter.butter_lowpass_filter(self.data, "Front Left", 4, 30, 2)
-        self.data = filter.butter_lowpass_filter(self.data, "Rear Right", 4, 30, 2)
-        self.data = filter.butter_lowpass_filter(self.data, "Rear Left", 4, 30, 2)
-    
-    def convert_to_gs(self) -> DataFrame:
-        # convert the voltage to gs
-        pass
-   
+        self.data = filter.butter_lowpass_filter(
+            self.data, "Front Right", 4, 30, 2)
+        self.data = filter.butter_lowpass_filter(
+            self.data, "Front Left", 4, 30, 2)
+        self.data = filter.butter_lowpass_filter(
+            self.data, "Rear Right", 4, 30, 2)
+        self.data = filter.butter_lowpass_filter(
+            self.data, "Rear Left", 4, 30, 2)
+
+    def convert_xl_g(self):
+        for i, row in self.xl_data.iterrows():
+            self.xl_data.loc[i, "X"] = (row["X Axis"] - self.x_basis) * 0.53
+            self.xl_data.loc[i, "Y"] = (row["Y Axis"] - self.y_basis) * 0.53
+            self.xl_data.loc[i, "Z"] = (row["Z Axis"] - self.z_basis) * 0.53
+
     def convert_time(self, data):
         for i, row in data.iterrows():
             time_step = row["Time"]

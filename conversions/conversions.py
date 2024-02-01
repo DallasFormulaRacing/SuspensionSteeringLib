d0f6e43951156d655d6f2912cebf11c881d5a4c3
JonathanXu42 <93541470+JonathanXu42@users.noreply.github.com>
2024-01-31 21:22:47 -0500

    Created two new functions for analog accelerometer data processing
    I created the clean_accelerometer_data() function, which uses a low pass filter to clean the data coming from the analog accelerometer.
    
    I created the convert_voltage_to_meters_per_sec_squared() function, which uses the newly added conversion offset, conversion constant, and Gs to m/s^2 conversion factor for the analog accelerometer, to convert voltage readings to m/s^2 of acceleration.

--- conversions/conversions.py
+++ conversions/conversions.py
@@ -59,3 +59,26 @@ class Conversions:
             data.loc[i, "Time"] = time.strftime(
                 "%H:%M:%S.{} %Z".format(mlsec), time.localtime(time_step)
             )
+
+    #Performing conversions on accelerometer data
+    def clean_accelerometer_data(self):
+        filter = Filter.filter()
+
+        #The two accelerometers are currently analog and are in the process of being switched out for digital ones.
+        #The butter_lowpass_filter() function uses a digital filter. I am not sure how significant the difference using an analog filter will be,
+        #but you can switch between the two by going to the butter_lowpass_filter() in Filter/filter.py, going to the line
+        #"b, a = butter(order, normal_cutoff, btype="low", analog=False)," and changing analog to True.
+
+        #self.data = filter.butter_lowpass_filter(self.data, name of the column as a str, non-normalized cutoff frequecy as a float, sampling frequency (fs) as a float, order as an int)
+        self.data = filter.butter_lowpass_filter(self.data, "X", 4, 30, 2)
+        self.data = filter.butter_lowpass_filter(self.data, "Y", 4, 30, 2)
+        self.data = filter.butter_lowpass_filter(self.data, "Z", 4, 30, 2)
+
+    def convert_voltage_to_meters_per_sec_squared(self):
+        constants = conversion_factor_enum.Constants
+        for i, row in self.data.iterrows():
+            #Each voltage reading in each axes first has the zero G voltage (1.5 V) subtracted from it. Then it is multiplied by the sensitivity factor (0.3 V/G), so that the product
+            #is in Gs of acceleration. Then, the Gs are converted to m/s^2.
+            self.data.loc[i, "X"] = (row["X"] - ANALOG_ACCEL_CONVERSION_OFFSET) / ANALOG_ACCEL_CONVERSION_CONSTANT * Gs_TO_METERS_PER_SEC_SQUARED_CONVERSION_FACTOR
+            self.data.loc[i, "Y"] = (row["Y"] - ANALOG_ACCEL_CONVERSION_OFFSET) / ANALOG_ACCEL_CONVERSION_CONSTANT * Gs_TO_METERS_PER_SEC_SQUARED_CONVERSION_FACTOR
+            self.data.loc[i, "Z"] = (row["Z"] - ANALOG_ACCEL_CONVERSION_OFFSET) / ANALOG_ACCEL_CONVERSION_CONSTANT * Gs_TO_METERS_PER_SEC_SQUARED_CONVERSION_FACTOR

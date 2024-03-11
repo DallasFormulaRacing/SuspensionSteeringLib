d2c89386b409a8e925191e495f5ab94d9267b937
GHAFHA <45609663+GHAFHA@users.noreply.github.com>
2024-03-11 16:18:09 -0500

    removed extra handler class

---
+++ calculations/constants.py
@@ -0,0 +1,37 @@
+from enum import Enum
+
+
+class Constants_23(float, Enum):
+    # Spring rates are in units: inches per pound
+    # Motion ratios are unitless
+    # Forces are in pound force (lbf)
+    SPRING_RATE_FRONT = 200.0
+    SPRING_RATE_REAR = 175.0
+    MOTION_RATIO_FRONT = 0.907
+    MOTION_RATIO_REAR = 0.87
+    SPRING_RATE_ROLL_FRONT = 0.0  # Value confirmed by Interim Suspension lead: Zheer
+    SPRING_RATE_ROLL_REAR = 0.0  # Value confirmed by Interim Suspension lead: Zheer
+    MOTION_RATIO_ROLL_FRONT = 0.2714
+    MOTION_RATIO_ROLL_REAR = 0.2852
+    CORNER_WEIGHT_LF = 150.0
+    CORNER_WEIGHT_RF = 150.0
+    CORNER_WEIGHT_RL = 150.0
+    CORNER_WEIGHT_RR = 150.0
+
+
+class Constants_24(float, Enum):
+    # Spring rates are in units: inches per pound
+    # Motion ratios are unitless
+    # Forces are in pound force (lbf)
+    SPRING_RATE_FRONT = 200.0
+    SPRING_RATE_REAR = 175.0
+    MOTION_RATIO_FRONT = 0.907
+    MOTION_RATIO_REAR = 0.87
+    SPRING_RATE_ROLL_FRONT = 0.0  # Value confirmed by Interim Suspension lead: Zheer
+    SPRING_RATE_ROLL_REAR = 0.0  # Value confirmed by Interim Suspension lead: Zheer
+    MOTION_RATIO_ROLL_FRONT = 0.2714
+    MOTION_RATIO_ROLL_REAR = 0.2852
+    CORNER_WEIGHT_LF = 150.0
+    CORNER_WEIGHT_RF = 150.0
+    CORNER_WEIGHT_RL = 150.0
+    CORNER_WEIGHT_RR = 150.0
